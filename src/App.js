d32b4ce89e11a4b4b6d956fb0498bd26d0be470a
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2023-01-26 16:20:44 -0500

    Asignacion por destructuring

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import Saludar from './components/Saludar';
 
 function App() {
 
-  // const username ="Jorge HN";
-  // const edad = 25;
-
-
   const user={
     nombre:"Juan HN",
     edad:18,
@@ -22,7 +18,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <Saludar name={username} edad={edad}/> */}
+
         <Saludar userInfo={user} saludarfn={saludarfn}/>
   
       </header>
--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,9 +1,13 @@
 import React from "react"
 
 export default function Saludar(props){
+    const {userInfo,saludarfn}= props
+    const {nombre} = userInfo
+    console.log(props)
+    console.log(userInfo)
     return (
         <div>
-        <button onClick={()=>props.saludarfn(props.userInfo.nombre)}>Saludar</button>
+        <button onClick={()=>saludarfn(nombre)}>Saludar</button>
         </div>
     );
 }
