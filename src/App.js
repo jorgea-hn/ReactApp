bb85a6dbbffb02e191e5b5314d721665690b8af7
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2023-01-27 08:47:45 -0500

    Instalacion y uso de react-boostrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Button, Accordion,Alert } from 'react-bootstrap';
+
+
 import './App.css';
-import Saludar from './components/Saludar';
 
 function App() {
 
-  const user={
-    nombre:"Juan HN",
-    edad:18,
-    color:"rojo"
-  }
-
-  const saludarfn = (nombre, edad)=>{
-    console.log("Hola " + nombre + "tienes" + edad);
-    console.log(`Hola ${nombre} tienes ${edad} años`)
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <h1>React Boostrap</h1>
+      <Button variant="secondary" onClick={() => console.log("Hola")} size="lg">
+        Block level button
+      </Button>
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
 
-        <Saludar userInfo={user} saludarfn={saludarfn}/>
-  
-      </header>
+
+      <Alert variant="primary">
+          This is a alert—check it out!
+      </Alert>
     </div>
   );
 }
